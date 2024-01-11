40bafb09913fea6bd64ad44993176c367ee7944d
Azie88 <obandoandrew8@gmail.com>
2024-01-11 12:14:49 +0300

    updated file path

--- app.py
+++ app.py
@@ -6,14 +6,15 @@ import os, joblib
 import re
 
 
-pipeline = joblib.load(r'toolkit\pipeline.joblib')
+file_path = os.path.abspath('toolkit/pipeline.joblib')
+pipeline = joblib.load(file_path)
 
 
 inputs = ['Origin_lat', 'Origin_lon', 'Destination_lat', 'Destination_lon',
        'Trip_distance', 'Speed', 'dewpoint_2m_temperature',
-       'mean_2m_air_temperature', 'pickup_weekday', 'pickup_weekofyear',
-       'pickup_hour', 'pickup_minute', 'pickup_week_hour', 'month',
-       'day_of_week', 'cluster_id', 'target_transformed', 'temperature_range',
+       'maximum_2m_air_temperature', 'mean_2m_air_temperature',
+       'minimum_2m_air_temperature', 'pickup_weekday', 'pickup_hour',
+       'pickup_minute', 'pickup_week_hour', 'cluster_id', 'temperature_range',
        'wind_speed', 'rain']
 
 def predict(*args, pipeline=pipeline, inputs=inputs):
@@ -43,28 +44,26 @@ with gr.Blocks(theme=gr.themes.Monochrome()) as app:
   gr.Markdown("""This app uses a machine learning model to predict the ETA of trips on the Yassir Hailing App.Refer to the expander at the bottom for more information on the inputs.""")
 
   with gr.Row():
-        origin_lat= gr.Slider(2.806,3.381,step = 0.01,interactive=True, value=2.806, label = 'origin_lat')
-        origin_lon = gr.Slider(36.589,36.820,step =0.01,interactive=True, value=36.589,label = 'origin_lon')
-        Destination_lat =gr.Slider(2.807,3.381,step = 0.1,interactive=True, value=2.81,label ='Destination_lat')
-        Destination_lon =gr.Slider(36.592,36.819,step = 0.1,interactive=True, value=36.596,label ='Destination_lon')
-        Trip_distance = gr.Slider(61,958,step =10,interactive=True, value=20,label = 'Trip_distance')
-        Speed = gr.Slider(0.300,85.000, step=0.01, interactive=True, value=20, label = 'Speed')
+        origin_lat= gr.Slider(2.806,3.381,step = 0.001,interactive=True, value=2.806, label = 'origin_lat')
+        origin_lon = gr.Slider(36.589,36.820,step =0.001,interactive=True, value=36.589,label = 'origin_lon')
+        Destination_lat =gr.Slider(2.807,3.381,step = 0.001,interactive=True, value=2.810,label ='Destination_lat')
+        Destination_lon =gr.Slider(36.592,36.819,step = 0.001,interactive=True, value=36.596,label ='Destination_lon')
+        Trip_distance = gr.Slider(61,958,step =10,interactive=True, value=100,label = 'Trip_distance')
+        Speed = gr.Slider(0.300,85.000, step=0.01, interactive=True, value=40, label = 'Speed')
         
   with gr.Column():
-    dewpoint_2m_temperature =gr.Slider(280.000, 288.000, step = 0.01,interactive=True, value=282.201,label ='dewpoint_2m_temperature')
-    mean_2m_air_temperature =gr.Slider(285.203, 291.110,step = 0.1,interactive=True, value=287.203,label ='mean_2m_air_temperature')
+    dewpoint_2m_temperature =gr.Slider(280.000, 288.000, step = 0.001,interactive=True, value=282.201,label ='dewpoint_2m_temperature')
+    maximum_2m_air_temperature =gr.Slider(288.319, 294.420,step = 0.001,interactive=True, value=290.203,label ='maximum_2m_air_temperature')
+    mean_2m_air_temperature =gr.Slider(285.203, 291.110,step = 0.01,interactive=True, value=287.203,label ='mean_2m_air_temperature')
+    minimum_2m_air_temperature =gr.Slider(282.348, 287.693,step = 0.01,interactive=True, value=285.203,label ='minimum_2m_air_temperature')
     pickup_weekday = gr.Dropdown([0,1,2,3,4,5,6],label ='pickup_weekday', value=3)
-    pickup_weekofyear = gr.Dropdown([47,48,49,50],label="pickup_weekofyear",value=3)
     pickup_hour = gr.Dropdown([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
                                ,label="pickup_hour",value=13)
     pickup_minute = gr.Slider(0, 59, step = 1,interactive=True, value=5,label ='pickup_minute')
     pickup_week_hour = gr.Slider(0, 167, step = 1,interactive=True, value=5,label ='pickup_week_hour')
-    month = gr.Dropdown([11,12],label = 'month')
-    day_of_week = gr.Dropdown([0,1,2,3,4,5,6],label ='day_of_week', value=3)
     cluster_id = gr.Dropdown([1,2,3,4,5,6,7],label="Cluster ID", value=4)
 
   with gr.Column():
-    target_transformed = gr.Slider(0, 336.426, step = 0.01,interactive=True, value=100,label ='target_transformed')
     temperature_range = gr.Slider(2.317, 9.166, step = 0.01,interactive=True, value=5,label='temperature_range')
     wind_speed = gr.Slider(0.803, 9.887, step = 0.01,interactive=True, value=5,label='wind_speed')
     rain = gr.Dropdown([0,1],label='rain')
@@ -86,9 +85,9 @@ with gr.Blocks(theme=gr.themes.Monochrome()) as app:
                     """)
   btn.click(fn = predict,inputs= [origin_lat, origin_lon, Destination_lat, Destination_lon,
        Trip_distance, Speed, dewpoint_2m_temperature,
-       mean_2m_air_temperature, pickup_weekday, pickup_weekofyear,
-       pickup_hour, pickup_minute, pickup_week_hour, month,
-       day_of_week, cluster_id, target_transformed, temperature_range,
+       maximum_2m_air_temperature, mean_2m_air_temperature,
+       minimum_2m_air_temperature, pickup_weekday, pickup_hour,
+       pickup_minute, pickup_week_hour, cluster_id, temperature_range,
        wind_speed, rain], outputs = output)
   
   app.launch(share = True, debug =True)
